0f3b75d83be65f09e9b47271c28041b62839a65e
bgfritz1 <bgfritz1@gmail.com>
2021-09-03 14:13:55 +0200

    commit before only running kmeans on CBC

--- scripts/FINAL_DataAnalysis.R
+++ scripts/FINAL_DataAnalysis.R
@@ -45,15 +45,25 @@ out_dir <- "./analysis/"
 # Not sure if the analysis will work with 
 # continuous/numeric metadata.
 
-factors<-c("Ulcer_duration_cat", "IDSA_SCORE_1to4", "not_healed0_healed1", 
-           "Diabetes_type", "PAD", "Acute0_Chronic1", "is_NEBSmallRNA")
+factors<-c("Ulcer_duration_cat", "IDSA_SCORE_1to4", "is_NEBSmallRNA")
 
 # Convert any numeric to factors
 metadata[,factors]<- apply(metadata[,factors], 2, as.factor)
 
-#Split data and metadata into main and validation(includes validation data)
 
-#Save full data for train/testing
+# Filtering ===================================================================
+
+# Filter out samples with less than 1M reads and remove from metata
+counts <- counts[,colSums(counts) > 1000000]
+metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
+
+# Remove MW_CW5 and MW_CW6, HH5 because it's an extreme outlier - Abnormally high counts for e.g. PADI3.
+
+counts<-counts[,!(colnames(counts) %in% c("HH5","MW_CW5","MW_CW6"))]
+metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
+
+#Split data and metadata into Main and Validation(Main + Validation)
+
 counts_validation <- counts 
 metadata_validation <- metadata
 
@@ -62,70 +72,19 @@ counts <- counts[,c(colnames(counts) %in% metadata$Sample_ID[metadata$Data_set =
 metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
 
 
-# Filtering ===================================================================
-
-# Filter out samples with less than 1M reads
-counts <- counts[,colSums(counts) > 1000000]
-
-# Remove the filtered samples from the metadata set
-metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
-
-# Remove HH5 because it's an extreme outlier - Abnormally high counts for e.g. PADI3.
-# Remove MW_CW5 and MW_CW6 because they're acute burn wounds
-
-counts<-counts[,colnames(counts)!="HH5" & colnames(counts)!="MW_CW5"& colnames(counts)!="MW_CW6"]
-metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
-
-counts_filtered <- counts #Save for plotting
-# =============================================================================
-# Differential Gene Expression (DESeq2) of metadata factors
-# for "CBC" data (Note: this data is referred to as "LHS" in manuscript.)
-# =============================================================================
-
-
-# Get counts for only CBC data
-counts_cbconly <- counts[, colnames(counts) %in% metadata$Sample_ID[metadata$Source=="CBC"]]
-
-# Only count mRNA coding regions on exons
-counts_cbconly <- filterCountsbyGeneType(counts_cbconly, annotation_path, "exon", "mRNA")
-
-# Generate the DESeq2 Data set for the CBC data
-
-CBC_DEseq2 <- run_DESeq2(metadata = subset(metadata, Source == "CBC"),
-                         counts = counts_cbconly,
-                         id_colname = "Sample_ID",
-                         metadata_vars = c("Ulcer_duration_cat","IDSA_SCORE_1to4","Acute0_Chronic1"),
-                         formula = ~ Ulcer_duration_cat + IDSA_SCORE_1to4 + Acute0_Chronic1)
-
-# Test for differential expression
-CBC_DEgenes_UlcerDuration<-DESeq2::results(CBC_DEseq2, contrast = c("Ulcer_duration_cat", "2", "0"))
-CBC_DEgenes_IDSAScore<-DESeq2::results(CBC_DEseq2, contrast = c("IDSA_SCORE_1to4", "4","2"))
-CBC_DEgenes_Acute0_Chronic1<-DESeq2::results(CBC_DEseq2, contrast = c("Acute0_Chronic1", "1", "0"))
-
-# Filter for genes with abs(log2FoldChange > 2) and padj < 0.05
-CBC_DEgenes_Acute0_Chronic1_sig <- filter_DESeq(CBC_DEgenes_Acute0_Chronic1, 2, 0.05)
-CBC_DEgenes_IDSAScore_sig <- filter_DESeq(CBC_DEgenes_IDSAScore, 2, 0.05)
-CBC_DEgenes_UlcerDuration_sig <- filter_DESeq(CBC_DEgenes_UlcerDuration, 2, 0.05)
-
-#==============================================================================
-# Batch Normalization and Differential Gene Expression (DESeq2) analysis 
-# of metadata factors for "Combined" data 
-#==============================================================================
+#====================================================================================
+# Batch Normalization and Differential Gene Expression (DESeq2) analysis of Metadata
+#====================================================================================
 
 # Batch Normalization ========================================================
 
-
-# Remove genes differentially expressed based on the "Endedness"
-# In the metadata, Endedness is either "PE" or "SE" and only the NEB Small RNA kit is "SE"
-
 counts_batchnorm <- remove_batch_effect(counts, metadata, ~is_NEBSmallRNA, 1)
 
-
-## DESeq2 ======================================================================
+# DESeq2 ======================================================================
 
 # Only count mRNA from exonic regions
 
-counts_batchnorm_mRNA <- filterCountsbyGeneType(counts_batchnorm, annotation_path, "exon", c("mRNA", "lnc_RNA", "pseudogene"))
+counts_batchnorm_mRNA <- filterCountsbyGeneType(counts_batchnorm, annotation_path, "exon", c("mRNA"))
 
 # Generate the DESeq2 Data set for the combined data set (Note: This excludes the MW and KK data bc they dont have
 # both Ulcer_duration_cat and IDSA_Score_1to4 metadata)
@@ -148,29 +107,6 @@ combined_DEgenes_IDSAScore_sig <- filter_DESeq(combined_DEgenes_IDSAScore, 2, 0.
 # Perform K-means clustering analysis 
 #===================================================================================
 
-## CBC data ================================================================
-
-# get CBC metadata 
-metadata_cbconly <- subset(metadata, Source=="CBC")
-
-# Normalize the counts 
-counts_cbconly_vst <- DESeq2::vst(as.matrix(counts_cbconly), blind = FALSE)
-
-# Remove genes with zero variance across all samples
-novar_filter <- apply(counts_cbconly_vst, 1, sd)
-novar_filter <- novar_filter == 0
-counts_cbconly_vst <- counts_cbconly_vst[!novar_filter, ]
-
-# Perform the kmeans clustering
-set.seed(seed) # Set Seed for reproducibility
-kmeans_cbc <- kmeans(t(counts_cbconly_vst), centers = 3, nstart = 25)
-print(kmeans_cbc$cluster)
-# Get the results 
-
-kmeans_groups_cbc <- data.frame(Sample_ID = names(kmeans_cbc$cluster), cluster_res_cbc = as.character(kmeans_cbc$cluster))
-
-## Combined Data =======================================================================
-
 #Normalize the counts 
 counts_batchnorm_vst <- DESeq2::vst(as.matrix(counts_batchnorm_mRNA))
 
@@ -185,24 +121,7 @@ kmeans_all <- kmeans(t(counts_batchnorm_vst), centers = 3, nstart = 25)
 
 kmeans_groups_all <- data.frame(Sample_ID = names(kmeans_all$cluster), cluster_res_all = as.character(kmeans_all$cluster))
 
-####  Fix kmeans cluster names #####################################
-kmeans_groups_cbc$cluster_res_cbc<-
-  lapply(kmeans_groups_cbc$cluster_res_cbc, function(cluster){
-    if(cluster=="1"){
-      cluster <- "3"
-    }
-    else if(cluster=="2"){
-      cluster <- "1"
-    }
-    else if(cluster=="3"){
-      cluster<-"2"
-    }
-    else{
-      stop("ERROR CONVERTING KMEANS GROUPS!!")
-    }
-  })
-
-kmeans_groups_cbc$cluster_res_cbc <- unlist(kmeans_groups_cbc$cluster_res_cbc)
+#Fix k-means cluster names 
 
 kmeans_groups_all$cluster_res_all<-
   lapply(kmeans_groups_all$cluster_res_all, function(cluster){
@@ -228,7 +147,6 @@ kmeans_groups_all$cluster_res_all <- unlist(kmeans_groups_all$cluster_res_all)
 
 # Add kmeans results to metadata 
 metadata_kmeans <- dplyr::left_join(metadata, kmeans_groups_all, by="Sample_ID")
-metadata_kmeans <- dplyr::left_join(metadata_kmeans, kmeans_groups_cbc, by="Sample_ID")
 
 # Run DESeq2
 dds_kmeans <- run_DESeq2(metadata = metadata_kmeans, 
@@ -258,19 +176,13 @@ DESeq_summary<-
                        combined_DEgenes_IDSAScore, #4vs2
                        DEseq_kmeans_all_3v2,
                        DEseq_kmeans_all_1v2,
-                       DEseq_kmeans_all_3v1,
-                       CBC_DEgenes_UlcerDuration, #2vs0
-                       CBC_DEgenes_IDSAScore, #4vs2 
-                       CBC_DEgenes_Acute0_Chronic1
-  ),
+                       DEseq_kmeans_all_3v1),
   names = c("combined_DEgenes_UlcerDuration", #2vs0
             "combined_DEgenes_IDSAScore", #4vs2
             "DEseq_kmeans_all_3v2",
             "DEseq_kmeans_all_1v2",
-            "DEseq_kmeans_all_3v1",
-            "CBC_DEgenes_UlcerDuration", #2vs0
-            "CBC_DEgenes_IDSAScore", #4vs2 
-            "CBC_DEgenes_Acute0_Chronic1"))#1vs0
+            "DEseq_kmeans_all_3v1"))
+
 
 #======================================================================
 # Analyze Kraken data to determine ratio of Bac/human reads 
@@ -363,82 +275,25 @@ high_bac_microbiome_top_species <-
     arrange(Sample_ID, desc(Rel_abund)) %>%
     filter(Rel_abund>1)
 
-
-
-
-ggsave("./analysis/Figures/plot_high_bac.pdf",
-       plot_high_bac, units = "mm", 
-       width = 120, 
-       height = 90, 
-       dpi = 300)
-
-
-#=======================================================================================
-# Analysis of Variance for IDSA_Score/High_bacteria/Cluster on Gene Expression per gene
-#=======================================================================================
-
-# Sample-Sample Correlation Matrix and Heirarchical clustering
-dist_mat <- dist(t(counts_batchnorm_vst), method = 'euclidian')
-hclust_avg <- hclust(dist_mat, method = 'average')
-plot(hclust_avg) #Note: HH28-P509 correspond to the samples with high bacteria! Highlight in figure
-
-var_counts <- counts_batchnorm_vst[,order(colnames(counts_batchnorm_vst))]
-var_metadata <- metadata_kmeans[order(metadata_kmeans$Sample_ID),]
-
-#Check that samples are the same
-
-all(colnames(var_counts) == var_metadata$Sample_ID)
-
-# Calculate percentage of variance due to IDSA_score and high percentage of bacteria for each gene
-
-variance_contribs<-
-   apply(var_counts, 1, fitmodel, IDSA_scores = var_metadata$IDSA_SCORE_1to4,
-         high_bacteria  = var_metadata$high_bacteria)
-
-# Format data for plotting 
-variance_contribs <- bind_rows(variance_contribs) 
-
-# Paired Wilcox Rank sum test to check if median % variability explained 
-# is different between the groups
-
-wilcox.test(unlist(variance_contribs[,1]),unlist(variance_contribs[,2]), paired = T)
-
 #=========================
 # Analyze Validation Data 
 #=========================
-
-# Filtering ====================
-
-# Filter out samples with less than 1M reads
-counts_validation <- counts_validation[,colSums(counts_validation) > 1000000]
-# Remove the filtered samples from the metadata set
-metadata_validation<-subset(metadata_validation, metadata_validation$Sample_ID %in% colnames(counts_validation))
-
-# Remove HH5 because it's an extreme outlier - Abnormally high counts for e.g. PADI3.
-# Including MW_CW5 and MW_CW6 because they're acute burn wounds - Added back in for evaluation
-
-counts_validation_filtered<-counts_validation[,colnames(counts_validation)!="HH5"]
-metadata_validation_filtered<-subset(metadata_validation, metadata$Sample_ID %in% colnames(counts_validation_filtered))
 
 # Normalization ====================
 
 # Only use genes which were used in main data
 
 unbiased_genes <- row.names(counts_batchnorm)
-counts_validation_batchnorm <- counts_validation_filtered[row.names(counts_validation_filtered) %in% unbiased_genes,]
-
-#Remove MW_CW5 and MW_CW6 and HH5
-counts_validation_batchnorm <- counts_validation_batchnorm[,!c(colnames(counts_validation_batchnorm) %in% c("HH5", "MW_CW5", "MW_CW6"))]
-metadata_validation_filtered <- metadata_validation_filtered[!metadata_validation_filtered$Sample_ID %in% c("HH5", "MW_CW5", "MW_CW6"),]
+counts_validation_batchnorm <- counts_validation[row.names(counts_validation) %in% unbiased_genes,]
 
 # Only count mRNA from exonic regions
-counts_validation_batchnorm_mRNA <- filterCountsbyGeneType(counts_validation_batchnorm, annotation_path, "exon", "mRNA")
+counts_validation_batchnorm_mRNA <- filterCountsbyGeneType(counts_validation_batchnorm, annotation_path, "exon", c("mRNA"))
 
 #Normalize the counts 
 counts_validation_batchnorm_vst <- DESeq2::vst(as.matrix(counts_validation_batchnorm_mRNA))
 
 #Add Kraken Data
-metadata_validation_filtered <- dplyr::left_join(metadata_validation_filtered, kraken_data, by="Sample_ID")
+metadata_validation_filtered <- dplyr::left_join(metadata_validation, kraken_data, by="Sample_ID")
 
 
 # Make Prediction Guesses 
@@ -462,6 +317,21 @@ prediction_guesses<-
   mutate( Specific_Type = ifelse(Type == "DFU" & Bac_prcnt < 10, "DFU - Not Infected", Specific_Type)) %>%
   mutate( Specific_Type = ifelse(Type == "DFU" & Bac_prcnt > 10, "DFU - Infected",  Specific_Type))
 
+#Guess if C1
+prediction_guesses<-
+prediction_guesses %>%
+  mutate(guess_isCluster_1 = ifelse(Type != "DFU" & Bac_prcnt < 10, "1", "0"))
+
+#Guess if C2
+
+prediction_guesses %>%
+  mutate(guess_isCluster_2 = ifelse(Type == "DFU" & Bac_prcnt < 10, "1", "0"))
+
+#Guess if C3
+prediction_guesses<-
+  prediction_guesses %>% 
+  mutate(guess_isCluster_3 = ifelse(Bac_prcnt > 10, "1", "0"))
+
 #Make sure validation output does not contain NAs
 
 
@@ -470,6 +340,10 @@ counts_validation_batchnorm_vst <- counts_validation_batchnorm_vst[,colnames(cou
                                                                      prediction_guesses$Sample_ID]
 
 prediction_guesses$is_infected <- ifelse(prediction_guesses$Bac_prcnt>10, "1", "0")
+
+# Add dummy for being in C3
+
+#metadata_validation_filtered$is_C3 <-ifelse(metadata_validation_filtered$cluster_res_all == "3", "yes", "no")
 
 #=========================================================================
 # Export gene counts data tables and analysis results to output directory
@@ -484,12 +358,8 @@ writeLines(capture.output(sessionInfo()), paste0(out_dir,"sessionInfo.txt"))
 
 # Export Counts
 write.csv(counts_batchnorm_vst, paste0(out_dir,"normalized_counts/Main_counts_batchnorm_vst.csv"))
-write.csv(counts_cbconly_vst, paste0(out_dir,"normalized_counts/counts_cbconly_vst.csv"))
 
 # Export DESeq results
-write.csv(CBC_DEgenes_Acute0_Chronic1_sig, paste0(out_dir,"DESeq2/CBC_DEgenes_Acute0_Chronic1.csv"))
-write.csv(CBC_DEgenes_IDSAScore_sig,paste0(out_dir,"DESeq2/CBC_DEgenes_IDSAScore.csv"))
-write.csv(CBC_DEgenes_UlcerDuration_sig,paste0(out_dir,"DESeq2/CBC_DEgenes_UlcerDuration.csv" ))
 write.csv(combined_DEgenes_UlcerDuration_sig,paste0(out_dir,"DESeq2/combined_DEgenes_UlcerDuration.csv" ))
 write.csv(combined_DEgenes_IDSAScore_sig,paste0(out_dir,"DESeq2/combined_DEgenes_IDSAScore.csv" ))
 write.csv(DEseq_kmeans_all_3v2_sig, paste0(out_dir,"DESeq2/DEseq_kmeans_all_3v2_sig.csv"))
@@ -502,7 +372,7 @@ write.csv(DESeq_summary, paste0(out_dir,"DESeq2/n_sig_DEgenes.csv"))
 
 # Export kmeans results
 write.csv(kmeans_groups_all, paste0(out_dir, "kmeans/kmeans_groups_all.csv"), row.names = FALSE)
-write.csv(kmeans_groups_cbc, paste0(out_dir, "kmeans/kmeans_groups_cbc.csv"), row.names = FALSE)
+
 
 # Export Data for GO_analysis
 
@@ -531,6 +401,6 @@ write.table(row.names(counts_batchnorm),
 #Export validation Data analysis
 
 write.csv(counts_validation_batchnorm_vst, "./data/Example_data/Validation_counts_batchnorm_vst.csv")
-write.csv(metadata_validation_filtered, "./data/Example_data/Validation_metadata.csv", row.names = F)
+write.csv(metadata_validation_filtered, "./data/Example_data/Validation_metadata.csv", row.names = F, quote = TRUE)
 write.csv(prediction_guesses, "./data/Validation_prediction_guesses", row.names = F)
 
