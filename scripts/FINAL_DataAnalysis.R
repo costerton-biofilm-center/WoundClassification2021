7ebe5e77a6fce7baab79977108d724dd014d20d0
bgfritz1 <bgfritz1@gmail.com>
2021-09-05 12:21:23 +0200

    seems to be working

--- scripts/FINAL_DataAnalysis.R
+++ scripts/FINAL_DataAnalysis.R
@@ -50,7 +50,6 @@ factors<-c("Ulcer_duration_cat", "IDSA_SCORE_1to4", "is_NEBSmallRNA")
 # Convert any numeric to factors
 metadata[,factors]<- apply(metadata[,factors], 2, as.factor)
 
-
 # Filtering ===================================================================
 
 # Filter out samples with less than 1M reads and remove from metata
@@ -62,13 +61,105 @@ metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
 counts<-counts[,!(colnames(counts) %in% c("HH5","MW_CW5","MW_CW6"))]
 metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
 
-#Split data and metadata into Main and Validation(Main + Validation)
+#======================================================================
+# Analyze Kraken data to determine ratio of Bac/human reads 
+#======================================================================
 
+library(tidyverse)
+
+kraken_data <- 
+  list.files(kraken_dir, full.names = T) %>%
+  set_names(., nm = map(.x = ., ~gsub(".kraken.report", "", basename(.x)))) %>%
+  map(function(x) {
+    x <- read.delim(x, header = F)
+    x$V6 <- trimws(x$V6, which = "left")
+    x <- subset(x, V6 %in% c("root", "Bacteria", "Homo sapiens"))
+    return(x)}) %>%
+  map_df(~as.data.frame(.x), .id = "Sample_ID") %>%
+  select(Sample_ID, "Percent_assigned" = V1, "Total_assigned" = V2, "Organism" = V6) %>%
+  group_by(Sample_ID) %>%
+  summarise(
+    "Total_reads" = Total_assigned[Organism=="root"],
+    "Bac_reads" = Total_assigned[Organism=="Bacteria"],
+    "Human_reads" = Total_assigned[Organism=="Homo sapiens"],
+    "Bac_prcnt" = Percent_assigned[Organism=="Bacteria"],
+    "Human_prcnt" = Percent_assigned[Organism=="Homo sapiens"])
+
+
+#Add kraken_results to kmeans metadata 
+metadata <- dplyr::left_join(metadata, kraken_data, by="Sample_ID")
+
+#Add Category for high bacteria to kmeans metadata 
+
+metadata$high_bacteria <- ifelse(metadata$Bac_prcnt>10, "high", "low")
+
+#=========================================================================
+# Analyze species and genera abundance from Kraken
+#=========================================================================
+
+#Note: Kraken files need to include Eukaryota, Bacteria, Viruses, and Archaea
+
+kraken_bac <- 
+  list.files(kraken_dir, full.names = T) %>%
+  set_names(., nm = map(.x = ., ~gsub(".kraken.report", "", basename(.x)))) %>%
+  map(function(x) {
+    #### Read in the files and format a bit 
+    x <- read.delim(x, header = F)
+    x$V6 <- trimws(x$V6, which = "left")
+    x <- x[, c(1,2,4,6)] #"Subset columns of interest"
+    colnames(x)<-c("Prcnt_root_taxon", "nr_reads_root_taxon", "Rank_code", "ID")
+    
+    #### We need to extract the bacteria info, but the order of the domains in the file are not consistent
+    #### So here is some ugly code to get only bacterial species
+    
+    domain_indices <- subset(x, Rank_code == "D") #get indexes of domain ranks 
+    domain_indices$index <-c(1:nrow(domain_indices)) #make a vector for indexing
+    
+    
+    bac_index <- domain_indices$index[domain_indices$ID =="Bacteria"] #Find which row is bacteria 
+    next_domain <- bac_index+1
+    
+    bac_index <- as.numeric(row.names(domain_indices)[bac_index])
+    
+    if(next_domain == nrow(domain_indices)){
+      stop_index <- nrow(x)
+    }
+    else{ 
+      stop_index <-as.numeric(row.names(domain_indices)[next_domain])-1
+    }
+    x <- x[bac_index:stop_index,]
+    x <- subset(x, Rank_code == "S")
+    return(x)}) %>%
+  bind_rows(., .id = "Sample_ID")
+
+
+kraken_bac_abundance <-
+  metadata[,c("Sample_ID","Bac_reads", "Human_reads")] %>%
+  left_join(kraken_bac, by = "Sample_ID") %>%
+  group_by(Sample_ID) %>%
+  mutate("Rel_abund" = nr_reads_root_taxon/sum(nr_reads_root_taxon)*100)
+
+
+
+# Get only samples with high bacteria 
+
+high_bac_microbiome <- 
+  subset(kraken_bac_abundance, Sample_ID %in% metadata$Sample_ID[metadata$Bac_prcnt>30]) 
+
+high_bac_microbiome_top_species <- 
+  high_bac_microbiome %>% 
+  group_by(Sample_ID) %>%
+  arrange(Sample_ID, desc(Rel_abund)) %>%
+  filter(Rel_abund>1)
+
+#=====================================================================================
+#Split data and metadata into Main and Validation(Main + Validation)
+#=====================================================================================
 counts_validation <- counts 
 metadata_validation <- metadata
 
 #Subset to only use "Main" data set 
-counts <- counts[,c(colnames(counts) %in% metadata$Sample_ID[metadata$Data_set == "Main"])]
+counts <- counts[,c(colnames(counts) %in% metadata$Sample_ID[metadata$Source %in% c("CBC")])]
 metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
 
 
@@ -78,8 +169,8 @@ metadata<-subset(metadata, metadata$Sample_ID %in% colnames(counts))
 
 # Batch Normalization ========================================================
 
-counts_batchnorm <- remove_batch_effect(counts, metadata, ~is_NEBSmallRNA, 1)
-
+#counts_batchnorm <- remove_batch_effect(counts, metadata, ~is_NEBSmallRNA, 1)
+counts_batchnorm <- counts
 # DESeq2 ======================================================================
 
 # Only count mRNA from exonic regions
@@ -93,7 +184,7 @@ combined_DESeq2 <- run_DESeq2(metadata = metadata,
                               counts = counts_batchnorm_mRNA,
                               id_colname = "Sample_ID",
                               metadata_vars = c("Ulcer_duration_cat","IDSA_SCORE_1to4", "Lib_prep"),
-                              formula = ~ Ulcer_duration_cat + IDSA_SCORE_1to4 + Lib_prep)
+                              formula = ~ Ulcer_duration_cat + IDSA_SCORE_1to4)
 
 # Test for differential expression between "extreme" values of metadata
 combined_DEgenes_UlcerDuration<-DESeq2::results(combined_DESeq2, contrast = c("Ulcer_duration_cat", "2", "0"))
@@ -152,8 +243,8 @@ metadata_kmeans <- dplyr::left_join(metadata, kmeans_groups_all, by="Sample_ID")
 dds_kmeans <- run_DESeq2(metadata = metadata_kmeans, 
                          counts = counts_batchnorm_mRNA,
                          id_colname = "Sample_ID",
-                         metadata_vars = c("cluster_res_all", "Lib_prep"),
-                         formula = ~ cluster_res_all + Lib_prep) 
+                         metadata_vars = c("Source","cluster_res_all"),
+                         formula = ~ cluster_res_all) 
 
 
 # Extract DE genes results
@@ -183,167 +274,14 @@ DESeq_summary<-
             "DEseq_kmeans_all_1v2",
             "DEseq_kmeans_all_3v1"))
 
+#=============================================================
+# Prepare Validation Data 
+#=============================================================
 
-#======================================================================
-# Analyze Kraken data to determine ratio of Bac/human reads 
-#======================================================================
+# Normalize for Library Prep Kit 
 
-library(tidyverse)
-
-kraken_data <- 
-  list.files(kraken_dir, full.names = T) %>%
-  set_names(., nm = map(.x = ., ~gsub(".kraken.report", "", basename(.x)))) %>%
-  map(function(x) {
-    x <- read.delim(x, header = F)
-    x$V6 <- trimws(x$V6, which = "left")
-    x <- subset(x, V6 %in% c("root", "Bacteria", "Homo sapiens"))
-    return(x)}) %>%
-  map_df(~as.data.frame(.x), .id = "Sample_ID") %>%
-  select(Sample_ID, "Percent_assigned" = V1, "Total_assigned" = V2, "Organism" = V6) %>%
-  group_by(Sample_ID) %>%
-  summarise(
-    "Total_reads" = Total_assigned[Organism=="root"],
-    "Bac_reads" = Total_assigned[Organism=="Bacteria"],
-    "Human_reads" = Total_assigned[Organism=="Homo sapiens"],
-    "Bac_prcnt" = Percent_assigned[Organism=="Bacteria"],
-    "Human_prcnt" = Percent_assigned[Organism=="Homo sapiens"])
-
-
-#Add kraken_results to kmeans metadata 
-metadata_kmeans <- dplyr::left_join(metadata_kmeans, kraken_data, by="Sample_ID")
-
-#Add Category for high bacteria to kmeans metadata 
-
-metadata_kmeans$high_bacteria <- ifelse(metadata_kmeans$Bac_prcnt>10, "high", "low")
-
-#=========================================================================
-# Analyze species and genera abundance from Kraken
-#=========================================================================
-
-#Note: Kraken files need to include Eukaryota, Bacteria, Viruses, and Archaea
-
-kraken_bac <- 
-  list.files(kraken_dir, full.names = T) %>%
-  set_names(., nm = map(.x = ., ~gsub(".kraken.report", "", basename(.x)))) %>%
-  map(function(x) {
-    #### Read in the files and format a bit 
-    x <- read.delim(x, header = F)
-    x$V6 <- trimws(x$V6, which = "left")
-    x <- x[, c(1,2,4,6)] #"Subset columns of interest"
-    colnames(x)<-c("Prcnt_root_taxon", "nr_reads_root_taxon", "Rank_code", "ID")
-    
-    #### We need to extract the bacteria info, but the order of the domains in the file are not consistent
-    #### So here is some ugly code to get only bacterial species
-    
-    domain_indices <- subset(x, Rank_code == "D") #get indexes of domain ranks 
-    domain_indices$index <-c(1:nrow(domain_indices)) #make a vector for indexing
-    
-
-    bac_index <- domain_indices$index[domain_indices$ID =="Bacteria"] #Find which row is bacteria 
-    next_domain <- bac_index+1
-    
-    bac_index <- as.numeric(row.names(domain_indices)[bac_index])
-    
-    if(next_domain == nrow(domain_indices)){
-      stop_index <- nrow(x)
-    }
-    else{ 
-      stop_index <-as.numeric(row.names(domain_indices)[next_domain])-1
-    }
-    x <- x[bac_index:stop_index,]
-    x <- subset(x, Rank_code == "S")
-    return(x)}) %>%
-   bind_rows(., .id = "Sample_ID")
-
-
-kraken_bac_abundance <-
-  metadata_kmeans[,c("Sample_ID","Bac_reads", "Human_reads")] %>%
-    left_join(kraken_bac, by = "Sample_ID") %>%
-    group_by(Sample_ID) %>%
-    mutate("Rel_abund" = nr_reads_root_taxon/sum(nr_reads_root_taxon)*100)
-
-
-
-# Get only samples with high bacteria 
-
-high_bac_microbiome <- 
-  subset(kraken_bac_abundance, Sample_ID %in% metadata_kmeans$Sample_ID[metadata_kmeans$Bac_prcnt>30]) 
-
-high_bac_microbiome_top_species <- 
-  high_bac_microbiome %>% 
-    group_by(Sample_ID) %>%
-    arrange(Sample_ID, desc(Rel_abund)) %>%
-    filter(Rel_abund>1)
-
-#=========================
-# Analyze Validation Data 
-#=========================
-
-# Normalization ====================
-
-# Only use genes which were used in main data
-
-unbiased_genes <- row.names(counts_batchnorm)
-counts_validation_batchnorm <- counts_validation[row.names(counts_validation) %in% unbiased_genes,]
-
-# Only count mRNA from exonic regions
-counts_validation_batchnorm_mRNA <- filterCountsbyGeneType(counts_validation_batchnorm, annotation_path, "exon", c("mRNA"))
-
-#Normalize the counts 
-counts_validation_batchnorm_vst <- DESeq2::vst(as.matrix(counts_validation_batchnorm_mRNA))
-
-#Add Kraken Data
-metadata_validation_filtered <- dplyr::left_join(metadata_validation, kraken_data, by="Sample_ID")
-
-
-# Make Prediction Guesses 
-
-prediction_guesses <- data.frame("Sample_ID" = metadata_validation_filtered$Sample_ID,
-                                 "Train_test" = metadata_validation_filtered$Source,
-                                 "Ulcer_duration_cat" = metadata_validation_filtered$Ulcer_duration_cat,
-                                 "Bac_prcnt" = metadata_validation_filtered$Bac_prcnt,
-                                 "Source" = metadata_validation_filtered$Source,
-                                 "Type" = metadata_validation_filtered$Type,
-                                 "IDSA_SCORE_1to4" = metadata_validation_filtered$IDSA_SCORE_1to4,
-                                 "Specific_Type" = metadata_validation_filtered$Specific_Type,
-                                 stringsAsFactors = F)
-
-prediction_guesses<-
-  prediction_guesses %>% 
-  mutate(Train_test = ifelse(Train_test == "CBC", "Train", "Test")) %>%
-  mutate( cluster_guess = ifelse(Type == "DFU" & Bac_prcnt > 10, "3", "")) %>%
-  mutate( cluster_guess = ifelse(Type == "DFU" & Bac_prcnt < 10, "2", cluster_guess)) %>%
-  mutate( cluster_guess = ifelse(Type != "DFU" & Bac_prcnt < 10, "1",  cluster_guess)) %>%
-  mutate( Specific_Type = ifelse(Type == "DFU" & Bac_prcnt < 10, "DFU - Not Infected", Specific_Type)) %>%
-  mutate( Specific_Type = ifelse(Type == "DFU" & Bac_prcnt > 10, "DFU - Infected",  Specific_Type))
-
-#Guess if C1
-prediction_guesses<-
-prediction_guesses %>%
-  mutate(guess_isCluster_1 = ifelse(Type != "DFU" & Bac_prcnt < 10, "1", "0"))
-
-#Guess if C2
-
-prediction_guesses %>%
-  mutate(guess_isCluster_2 = ifelse(Type == "DFU" & Bac_prcnt < 10, "1", "0"))
-
-#Guess if C3
-prediction_guesses<-
-  prediction_guesses %>% 
-  mutate(guess_isCluster_3 = ifelse(Bac_prcnt > 10, "1", "0"))
-
-#Make sure validation output does not contain NAs
-
-
-prediction_guesses <- prediction_guesses[!is.na(prediction_guesses$cluster_guess),]
-counts_validation_batchnorm_vst <- counts_validation_batchnorm_vst[,colnames(counts_validation_batchnorm_vst) %in%
-                                                                     prediction_guesses$Sample_ID]
-
-prediction_guesses$is_infected <- ifelse(prediction_guesses$Bac_prcnt>10, "1", "0")
-
-# Add dummy for being in C3
-
-#metadata_validation_filtered$is_C3 <-ifelse(metadata_validation_filtered$cluster_res_all == "3", "yes", "no")
+counts_validation_mRNA <- filterCountsbyGeneType(counts_validation, annotation_path, "exon", c("mRNA"))
+counts_validation_mRNA_vst <- DESeq2::vst(as.matrix(counts_validation))
 
 #=========================================================================
 # Export gene counts data tables and analysis results to output directory
@@ -400,7 +338,7 @@ write.table(row.names(counts_batchnorm),
 
 #Export validation Data analysis
 
-write.csv(counts_validation_batchnorm_vst, "./data/Example_data/Validation_counts_batchnorm_vst.csv")
-write.csv(metadata_validation_filtered, "./data/Example_data/Validation_metadata.csv", row.names = F, quote = TRUE)
-write.csv(prediction_guesses, "./data/Validation_prediction_guesses", row.names = F)
+write.csv(counts_validation_vst, "./data/Example_data/Validation_counts_batchnorm_vst.csv")
+write.csv(metadata_validation_mRNA_vst, "./data/Example_data/Validation_metadata.csv", row.names = F, quote = TRUE)
+
 
---
+++ scripts/clean_ML_classifier.py
@@ -0,0 +1,188 @@
+import numpy as np
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC, LinearSVC
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import cross_val_score, StratifiedKFold, ShuffleSplit, KFold
+from tqdm import tqdm
+import pdb 
+
+def main():
+	# Output dir 
+	output_dir = "./analysis/linearSVM_out"
+
+	# Data Paths
+	counts_path = "./data/Example_data/Validation_counts_batchnorm_vst.csv"
+	metadata_path = "./data/Example_data/Validation_metadata.csv"
+	kmeans_path = "./analysis/kmeans/kmeans_groups_all.csv" 
+
+	# Get the data 
+	count_data = pd.read_csv(counts_path, index_col=0, sep = ",")
+	metadata = pd.read_csv(metadata_path, sep = ",")
+	kmeans_data = pd.read_csv(kmeans_path, dtype = np.str)
+
+	# Merge kmeans with metadata
+	metadata = metadata.merge(kmeans_data, on = "Sample_ID", how = "left")
+
+	#Filter metdata, format variables, make dummies 
+	metadata = metadata[["Sample_ID", "Source", "Data_set", "IDSA_SCORE_1to4", "cluster_res_all"]]
+	dummies = pd.get_dummies(metadata["cluster_res_all"], prefix = "isCluster")
+
+	#Combine the data'
+	metadata = pd.concat([metadata, dummies], axis=1)
+	
+	#Select training categories
+	categories = ["IDSA_SCORE_1to4", "isCluster_1", "isCluster_2" , "isCluster_3"]
+
+	#Feature Selection 
+
+	for cat in categories: 
+		#Make sure the data is clean & only from CBC 
+		cleaned_data = clean_data(count_data, metadata[metadata['Source'] == 'CBC'], "Sample_ID", cat)
+
+		#Fit the model 
+		model = LinearSVC(C=1, penalty='l1', dual=False, max_iter=100000)
+		model.fit(np.array(cleaned_data[0]).T, np.array(cleaned_data[1][cat], dtype = np.int)) 
+		
+		#Get the good predictor genes
+		model_fselect = SelectFromModel(model, prefit=True, threshold=-np.inf, max_features=20)
+		good_genes = count_data.index[model_fselect.get_support()]
+		
+		print(f"For {cat}, the good genes are : {good_genes}")
+
+		plot_nFeatVsAcc(cleaned_data[0], cleaned_data[1], cat, n_features = 100, save = True)
+
+		#Make a mask for good genes 
+		mask = np.array([1 if x in good_genes else 0 for x in count_data.index]) == 1
+
+		#Export the results
+		for i in range(0, len(model.classes_)):
+			cat_name = model.classes_[i] #So that the names are correct
+			out_goodgenes = pd.DataFrame(good_genes, columns = ["Predictor Genes"])
+			out_goodgenes_filename = f"{output_dir}/GoodGenes_{cat}.csv"
+			out_goodgenes.to_csv(out_goodgenes_filename, index = False)
+
+		#If using linearSVC, can remove comments and get coefficients/plots 
+		for i in range(0,len(model.coef_)):
+			cat_level = model.classes_[i]
+			model_coefs = model.coef_[i]
+
+			#Mask unused coefs
+			masked_coefs = model.coef_[i,mask]
+            
+			#Define title and make plots
+			title = f"{cat}: {cat_level}"
+			plot_fimportances(masked_coefs, good_genes, cat=title)
+            
+			#Output the data:
+			out_data = np.column_stack((good_genes, masked_coefs))
+			out_file = f"{output_dir}/COEF_{cat}_{cat_level}.csv"
+			out_data = pd.DataFrame(out_data, columns = ["Genes", "Coefficient"])
+			out_data = out_data.sort_values("Coefficient", ascending = False)
+			out_data.to_csv(out_file, index = False)
+
+		# Train and fit using only good genes
+		model_validation = SVC(probability=True)
+		model_validation.fit(np.array(cleaned_data[0][mask]).T, np.array(cleaned_data[1][cat], dtype = np.int)) 
+
+		# Predict on test data
+		test_samples = metadata['Sample_ID'][metadata['Data_set'] == 'Validation']
+		test_counts = count_data.filter(items = test_samples)[mask]
+
+		prediction = model_validation.predict(np.array(test_counts).T)
+		prediction_prob = model_validation.predict_proba(np.array(test_counts).T)
+
+		results = pd.DataFrame({'Sample_ID':test_counts.keys(), \
+								'prediction': prediction.tolist(),\
+								'prediction_proba' : prediction_prob.tolist()})
+        
+		results.to_csv(f"./analysis/validation/{cat}_predictions.csv", index = False)
+
+def clean_data(data, metadata, sample_id_colname, classifier_term, subset = False ):
+    """ This function will take a rna-seq count data set, metadata,
+        and identifying col name to remove samples from metadata that aren't
+        in the main data. 
+        It returns a list where the first element is the metadata and the second is
+        the metadata.
+        It will also make sure the metadata and count data are in the proper order 
+        Note: If classifier term, it returns the data as np arrays instead of pandas(actally doesnt 
+        look like this is true )
+    """
+
+    #Remove samples from metadata which aren't in the data
+    metadata = metadata.loc[metadata[sample_id_colname].isin(data.columns)]
+    data = data.loc[:, data.columns.isin(metadata[sample_id_colname])]
+
+    if subset: 
+        metadata = metadata.query(subset)
+    
+    if classifier_term:
+        metadata = metadata.query(f"{classifier_term} != 'NA' & {classifier_term} != 'NaN'" )
+        data = data.loc[:, data.columns.isin(metadata[sample_id_colname])]
+
+    #Fix order or data and metadata 
+    data = data.reindex(sorted(data.columns), axis = 1)
+    metadata = metadata.sort_values(by=["Sample_ID"])
+
+    cols_equal = np.array_equal(data.columns, metadata["Sample_ID"])
+
+    if cols_equal:
+        return([data, metadata])
+
+    else:
+        print("ERROR: Orders of samples in metadata/data don't match")
+        sys.exit()
+
+def plot_fimportances(coef, f_names, cat):
+	imp = coef
+	imp,f_names = zip(*sorted(zip(imp,f_names))) #Added 0 to imp and it worked
+	plt.barh(range(len(f_names)), imp, align='center')
+	plt.yticks(range(len(f_names)), f_names)
+	plt.title(cat)
+	plt.tight_layout(pad=1)
+	#plt.show()
+
+def plot_nFeatVsAcc(counts, metadata, cat, n_features=10, save = False):
+
+	print("Now Testing the accuracy for different #'s of features")
+
+	model_accuracy = []
+	feature_nr = []
+
+	#Run the models and get the accuracy
+	for n_feature in tqdm(range(1,n_features)):
+		# Get Good Genes
+		model = LinearSVC(C=1, penalty='l1', dual=False, max_iter=100000, random_state = 15815)
+		model.fit(np.array(counts).T, np.array(metadata[cat], dtype = np.int)) 
+		model_fselect = SelectFromModel(model, prefit=True, threshold=-np.inf, max_features=n_feature)
+		good_genes = counts.index[model_fselect.get_support()]
+
+		#Mask non-useful genes from train/test data
+		mask = np.array([1 if x in good_genes else 0 for x in counts.index]) == 1
+
+		#Fit training data 
+		model = SVC(C=1, random_state = 15815)    
+		
+		# Split test and train 
+		skf = StratifiedKFold(n_splits = 2)
+
+		#Cross validation
+		scores = cross_val_score(model, counts[mask].T, metadata[cat], cv = skf)
+		
+		model_accuracy.append(np.mean(scores))
+		feature_nr.append(n_feature)
+
+    #Make the figure
+	fig, ax = plt.subplots()
+	ax.plot(feature_nr, model_accuracy)
+	ax.set(xlabel = 'Number of Genes in Classifier', ylabel = 'Classifier Accuracy')
+	ax.set_ylim(0.25,1.1)
+	plt.yticks(np.arange(0,1.2,0.2))
+	#plt.show()
+	
+	if save:
+		fig.savefig(f"./analysis/Figures/Accuracy_{cat}.png", dpi = 300, bbox_inches = 'tight')
+
+if __name__ == "__main__":
+	main()
